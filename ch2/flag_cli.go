c2250b967d921dacb8b4f6a1c46d6659ad4d26f0
Matt Holloway <hollowmatt@google.com>
2023-10-26 15:39:35 +0000

    flags

---
+++ ch2/flag_cli.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+var name = flag.String("name", "World", "A name to say hello to.")
+var spanish bool
+var french bool
+
+func init() {
+	flag.BoolVar(&spanish, "spanish", false, "Use Spanish language")
+	flag.BoolVar(&spanish, "s", false, "Use Spanish language")
+	flag.BoolVar(&french, "french", false, "Use French language")
+	flag.BoolVar(&french, "f", false, "Use French language")
+}
+
+func main() {
+	flag.Parse()
+
+	if spanish == true {
+		fmt.Printf("Hola %s!\n", *name)
+	} else {
+		if french == true {
+			fmt.Printf("Salut %s!\n", *name)
+		} else {
+			fmt.Printf("Hello %s!\n", *name)
+		}
+	}
+}
