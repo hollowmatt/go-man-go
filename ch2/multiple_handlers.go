b623e0bd289ad4ed06e71b6403fc6844fee52e7c
Matt Holloway <hollowmatt@gmail.com>
2023-11-17 14:08:21 +0000

    add goodbye path

--- ch2/multiple_handlers.go
+++ ch2/multiple_handlers.go
@@ -3,16 +3,28 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func main() {
 	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/goodbye/", goodbye)
 	http.ListenAndServe(":8080", nil)
 }
 
 func hello(res http.ResponseWriter, req *http.Request) {
 	query := req.URL.Query()
 	name := query.Get("name")
+	if name == "" {
+		name = "Inigo Montoya"
+	}
+	fmt.Fprint(res, "Hello, my name is ", name)
+}
+
+func goodbye(res http.ResponseWriter, req *http.Request) {
+	path := req.URL.Path
+	parts := strings.Split(path, "/")
+	name := parts[2]
 	if name == "" {
 		name = "Inigo Montoya"
 	}
