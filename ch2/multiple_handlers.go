c50bb7942d0f68bc2a1c823b41bdf1b4299ba230
Matt Holloway <hollowmatt@gmail.com>
2023-11-17 14:03:56 +0000

    add hello function

--- ch2/multiple_handlers.go
+++ ch2/multiple_handlers.go
@@ -3,9 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"strings"
 )
 
 func main() {
-	
+	http.HandleFunc("/hello", hello)
+	http.ListenAndServe(":8080", nil)
+}
+
+func hello(res http.ResponseWriter, req *http.Request) {
+	query := req.URL.Query()
+	name := query.Get("name")
+	if name == "" {
+		name = "Inigo Montoya"
+	}
+	fmt.Fprint(res, "Goodbye ", name)
 }
